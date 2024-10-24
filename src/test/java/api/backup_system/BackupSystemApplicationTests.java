501b50aa4ba3205214e25c56cd2874c1f5155e52
GabrielL915 <gabrielluizgomes1@gmail.com>
2024-10-24 15:28:12 -0300

    create project

---
+++ src/test/java/api/backup_system/BackupSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package api.backup_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackupSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
